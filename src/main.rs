8b16df1047b6137482bd2ed0b64ee7ba2ccfe1c4
Liam Woodward <liamowoodward@gmail.com>
2020-02-27 20:51:37 -0500

    initial commit, MVP

---
+++ src/main.rs
@@ -0,0 +1,387 @@
+// Get rid of dead code warnings for now
+#![allow(dead_code)]
+
+mod db;
+mod models;
+
+use std::path::{Path, PathBuf};
+use std::{env, fs, process};
+
+use db::{Field, Query, Table, TableError, Where};
+use models::{DbContext, Note};
+
+use chrono::{DateTime, Local};
+use directories::ProjectDirs;
+use exitfailure::ExitFailure;
+use failure::ResultExt;
+use structopt::StructOpt;
+
+/*
+
+notes new -f note.txt
+notes new "Day 12" "Today in my diary..."
+
+*/
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "notes")]
+enum Opt {
+    /// Create a new note.
+    #[structopt(name = "new")]
+    New {
+        /// Create a new note from file
+        #[structopt(short, long, conflicts_with = "editor")]
+        file: Option<PathBuf>,
+        /// Open new file with given title in default editor, use contents to create a new note
+        #[structopt(short, long, conflicts_with = "file")]
+        editor: Option<String>,
+        #[structopt(name = "title", required_unless_one = &["file", "editor"], conflicts_with_all = &["file", "editor"])]
+        title: Option<String>,
+        #[structopt(name = "text", required_unless_one = &["file", "editor"], conflicts_with_all = &["file", "editor"])]
+        text: Option<String>,
+    },
+    /// Retrieve an existing note.
+    Get {
+        /// Returns all notes
+        #[structopt(short, long)]
+        all: bool,
+        /// Only return notes that exactly match the designated title
+        #[structopt(short, long, requires = "title")]
+        exact: bool,
+        /// Returns first note that matches the title, or all notes if `all` is specified.
+        #[structopt(short, long, required_unless = "all")]
+        title: Option<String>,
+    },
+    Edit {
+        #[structopt(name = "note_title")]
+        note_title: String,
+        #[structopt(long, required_unless = "text")]
+        title: Option<String>,
+        #[structopt(long, required_unless = "title")]
+        text: Option<String>,
+    },
+    /// Delete an existing note.
+    Delete {
+        /// Deletes all notes
+        #[structopt(short, long)]
+        all: bool,
+        /// Only deletes notes that exactly match the designated title
+        #[structopt(short, long, requires = "title")]
+        exact: bool,
+        /// Deletes first note that matches the title, or all notes that match the title if `all` is specified.
+        #[structopt(short, long, required_unless = "all")]
+        title: Option<String>,
+    },
+}
+
+fn get_notes_with_title(
+    db_context: &DbContext,
+    title: String,
+    exact: bool,
+) -> Result<Vec<Note>, TableError> {
+    match exact {
+        false => {
+            let field = format!("%{}%", title);
+            let query = Query::new_get(db_context.table.clone(), vec![title])
+                .add_where(Where::Like("title".to_string(), Field::Str(field)));
+            Note::get(db_context.connection(), query)
+        }
+        true => {
+            let field = format!("{}", title);
+            let query = Query::new_get(db_context.table.clone(), vec![title])
+                .add_where(Where::Equal("title".to_string(), Field::Str(field)));
+            Note::get(db_context.connection(), query)
+        }
+    }
+}
+
+fn delete_notes_with_title(
+    db_context: &DbContext,
+    title: &str,
+    exact: bool,
+) -> Result<u32, TableError> {
+    match exact {
+        false => {
+            let field = format!("%{}%", title);
+            let query = Query::new_delete(db_context.table.clone())
+                .add_where(Where::Like("title".to_string(), Field::Str(field)));
+            Note::delete(db_context.connection(), query)
+        }
+        true => {
+            let field = format!("{}", title);
+            let query = Query::new_delete(db_context.table.clone())
+                .add_where(Where::Equal("title".to_string(), Field::Str(field)));
+            Note::delete(db_context.connection(), query)
+        }
+    }
+}
+
+fn file_is_dir<P: AsRef<Path>>(path: P) -> Result<bool, ExitFailure> {
+    let metadata = fs::metadata(path)
+        .with_context(|e| format!("could not determine if file is directory: {}", e))?;
+    Ok(metadata.is_dir())
+}
+
+fn get_time_created<P: AsRef<Path>>(path: P) -> Result<DateTime<Local>, ExitFailure> {
+    let metadata =
+        fs::metadata(path).with_context(|e| format!("could not get file creation time: {}", e))?;
+    let time = metadata.created();
+
+    match time {
+        Ok(time) => Ok(DateTime::<Local>::from(time)),
+        Err(e) => Err(e.into()),
+    }
+}
+
+fn get_file_contents<P: AsRef<Path>>(path: P) -> Result<String, ExitFailure> {
+    let contents = fs::read_to_string(&path)
+        .with_context(|e| format!("could not get file contents: {}", e))?;
+    Ok(contents)
+}
+
+fn main() -> Result<(), ExitFailure> {
+    let opt = Opt::from_args();
+
+    // println!("{:?}", opt);
+
+    let project_dir = match ProjectDirs::from("", "", "Notes") {
+        None => Err(failure::err_msg("Could not open local data directory."))
+            .context("Could not get access to database.")?,
+        Some(d) => d,
+    };
+
+    let dir = project_dir.data_local_dir();
+
+    if !dir.exists() {
+        fs::create_dir(dir).with_context(|_| {
+            format!(
+                "could not create local data directory: `{}`",
+                dir.to_str().unwrap()
+            )
+        })?;
+    }
+
+    let path = dir.join("notes.db");
+    let mut db_context = DbContext::new(&path, "notes".to_string()).with_context(|e| {
+        format!(
+            "could not establish connection to database at `{}`: {}",
+            path.as_path().to_str().unwrap(),
+            e
+        )
+    })?;
+
+    let table = db_context.table.clone();
+
+    Note::init_db(db_context.connection())
+        .with_context(|e| format!("could not initiate database contents: {}", e))?;
+
+    match opt {
+        Opt::New {
+            file: None,
+            editor: None,
+            title: Some(title),
+            text: Some(text),
+        } => {
+            let tx = db_context
+                .transaction()
+                .with_context(|_| format!("could not create database transaction"))?;
+            Note::insert(&tx, table.clone(), Note::new(&title, &text))
+                .with_context(|e| format!("could not add note: {}", e))?;
+            tx.commit()?;
+        }
+        Opt::New {
+            file: Some(file),
+            editor: None,
+            title: None,
+            text: None,
+        } => {
+            let path = env::current_dir()
+                .context("Could not find file")?
+                .join(file);
+
+            if file_is_dir(&path)? {
+                panic!("File is a directory!");
+            }
+
+            // TODO: Remove expects later
+            let title = String::from(
+                path.file_stem()
+                    .expect("Could not get file name.")
+                    .to_str()
+                    .expect("Could not get file name."),
+            );
+            let created = get_time_created(&path)?;
+            let text = get_file_contents(&path)?.to_string();
+
+            let tx = db_context
+                .transaction()
+                .with_context(|_| format!("could not create database transaction"))?;
+            let note = Note {
+                title,
+                created,
+                text,
+            };
+            Note::insert(&tx, table.clone(), note)
+                .with_context(|e| format!("could not add note: {}", e))?;
+            tx.commit()
+                .context(format!("could not commit database transaction"))?;
+        }
+        Opt::New {
+            file: None,
+            editor: Some(title),
+            title: None,
+            text: None,
+        } => {
+            // https://stackoverflow.com/questions/56011927/how-do-i-use-rust-to-open-the-users-default-editor-and-get-the-edited-content
+            let editor = env::var("EDITOR")
+                .with_context(|_| format!("could not get default editor from environment"))?;
+            let mut file_path = env::temp_dir();
+            file_path.push("temp");
+            fs::File::create(&file_path)
+                .with_context(|e| format!("could not create temporary file to edit: {}", e))?;
+
+            process::Command::new(editor)
+                .arg(&file_path)
+                .status()
+                .with_context(|e| format!("error editing file: {}", e))?;
+
+            let contents = fs::read_to_string(&file_path)
+                .with_context(|e| format!("could not get file contents: {}", e))?;
+
+            let tx = db_context
+                .transaction()
+                .with_context(|_| format!("could not create database transaction"))?;
+
+            let note = Note::new(&title, &contents);
+            Note::insert(&tx, table.clone(), note)
+                .with_context(|e| format!("could not add note: {}", e))?;
+
+            tx.commit()
+                .context(format!("could not commit database transaction"))?;
+        }
+        // Gets all notes with matching title
+        Opt::Get {
+            all: true,
+            exact,
+            title: Some(title),
+        } => {
+            let notes =
+                get_notes_with_title(&db_context, title.clone(), exact).with_context(|e| {
+                    format!("could not get notes matching title `{}`: {}", title, e)
+                })?;
+
+            if notes.len() == 0 {
+                println!("No matches found.");
+            }
+
+            for note in notes {
+                println!("{}", note);
+            }
+        }
+        // Gets all notes
+        Opt::Get {
+            all: true,
+            exact: false,
+            title: None,
+        } => {
+            for row in Note::get_all(db_context.connection(), table.clone())
+                .with_context(|e| format!("could not get all notes: {}", e))?
+            {
+                println!("{}", row);
+            }
+        }
+        // Gets first note with matching title
+        Opt::Get {
+            all: false,
+            exact,
+            title: Some(title),
+        } => {
+            let notes =
+                get_notes_with_title(&db_context, title.clone(), exact).with_context(|e| {
+                    format!("could not get first note matching title `{}`: {}", title, e)
+                })?;
+            if let Some(note) = notes.get(0) {
+                println!("{}", note);
+            } else {
+                return Err(failure::err_msg(format!(
+                    "No notes matching title `{}` found.",
+                    title
+                )))
+                .context("Could not get note.")?;
+            }
+        }
+        Opt::Edit {
+            note_title,
+            title,
+            text,
+        } => {
+            let params = match (title, text) {
+                (Some(tit), Some(tex)) => {
+                    vec![("title".to_string(), tit), ("text".to_string(), tex)]
+                }
+                (Some(tit), None) => vec![("title".to_string(), tit)],
+                (None, Some(tex)) => vec![("text".to_string(), tex)],
+                (None, None) => {
+                    return Err(failure::err_msg("No title or text to change given"))
+                        .context("Could not change text.")?;
+                }
+            };
+
+            let query = Query::new_update(table.clone(), params)
+                .add_where(Where::Equal("title".to_string(), Field::Str(note_title)));
+
+            let tx = db_context
+                .transaction()
+                .with_context(|_| format!("could not create database transaction"))?;
+
+            Note::update(&tx, query).with_context(|e| format!("could not update note: {}", e))?;
+
+            tx.commit()
+                .context(format!("could not commit database transaction"))?;
+        }
+        Opt::Delete {
+            all: true,
+            exact,
+            title: Some(title),
+        } => {
+            let num_deleted =
+                delete_notes_with_title(&db_context, &title, exact).with_context(|e| {
+                    format!("could not delete notes matching title `{}`: {}", title, e)
+                })?;
+
+            println!("Deleted {} notes", num_deleted);
+        }
+        // Gets all notes
+        Opt::Delete {
+            all: true,
+            exact: false,
+            title: None,
+        } => {
+            for row in Note::get_all(db_context.connection(), table.clone())
+                .with_context(|e| format!("could not get all notes: {}", e))?
+            {
+                println!("{}", row);
+            }
+        }
+        // Gets first note with matching title
+        Opt::Delete {
+            all: false,
+            exact,
+            title: Some(title),
+        } => {
+            let num_deleted =
+                delete_notes_with_title(&db_context, &title, exact).with_context(|e| {
+                    format!(
+                        "could not delete first note matching title `{}`: {}",
+                        title, e
+                    )
+                })?;
+
+            println!("Deleted {} notes.", num_deleted);
+        }
+        _ => Err(failure::err_msg("Could not execute command"))
+            .context("Invalid command entered.")?,
+    }
+
+    Ok(())
+}
---
+++ src/models.rs
@@ -0,0 +1,250 @@
+use std::path::Path;
+
+use crate::db::*;
+
+use chrono::{DateTime, Local};
+use rusqlite::{params, Connection, Transaction, NO_PARAMS};
+
+/*
+
+let query = Query::new()
+    .add_where(Equal("title", "Day 12"))
+    .add_order(Ascending("noteid"))
+    .add_limit(10)
+
+for row in Note::query(conn, query) {
+    println("{}", row.unwrap())
+}
+
+*/
+
+// NOTE: REMEMBER to row.get(n) for the correct n; n=0 is the primary key row id
+
+pub struct DbContext {
+    pub table: TableName,
+    conn: Connection,
+}
+
+impl DbContext {
+    pub fn new<P: AsRef<Path>>(path: P, table: TableName) -> Result<Self, TableError> {
+        Ok(DbContext {
+            table,
+            conn: Connection::open(path)?,
+        })
+    }
+
+    pub fn connection(&self) -> &Connection {
+        &self.conn
+    }
+
+    pub fn transaction(&mut self) -> Result<Transaction, TableError> {
+        Ok(self.conn.transaction()?)
+    }
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Note {
+    pub title: String,
+    pub created: DateTime<Local>,
+    pub text: String,
+}
+
+impl Note {
+    pub fn new(title: &str, text: &str) -> Note {
+        Note {
+            title: title.to_string(),
+            created: Local::now(),
+            text: text.trim().to_string(),
+        }
+    }
+
+    pub fn init_db<'a>(conn: &Connection) -> Result<(), TableError> {
+        conn.execute(
+            "CREATE TABLE IF NOT EXISTS notes (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                title TEXT NOT NULL UNIQUE,
+                created TEXT NOT NULL,
+                text TEXT NOT NULL
+                )",
+            NO_PARAMS,
+        )?;
+
+        Ok(())
+    }
+}
+
+impl std::fmt::Display for Note {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let date = self.created.format("%Y-%m-%d %H:%M:%S").to_string();
+        write!(f, "[{}] {}\n\"{}\"", date, self.title, self.text)
+    }
+}
+
+impl Table for Note {
+    type Row = Note;
+
+    fn get_all(conn: &Connection, table: TableName) -> Result<Vec<Note>, TableError> {
+        Ok(conn
+            .prepare(&format!("SELECT * FROM {}", table))?
+            .query_map(NO_PARAMS, |row| {
+                Ok(Note {
+                    title: row.get(1)?,
+                    created: row.get(2)?,
+                    text: row.get(3)?,
+                })
+            })?
+            .filter_map(Result::ok)
+            .collect())
+    }
+
+    fn insert(conn: &Connection, table: TableName, row: Note) -> Result<(), TableError> {
+        conn.execute(
+            &format!(
+                "INSERT INTO {} (title, created, text) VALUES (?1, ?2, ?3)",
+                table
+            ),
+            params![row.title, row.created, row.text],
+        )?;
+        Ok(())
+    }
+
+    fn delete(conn: &Connection, query: Query) -> Result<u32, TableError> {
+        let query_string = &query.to_sql();
+        Ok(conn.execute(query_string, NO_PARAMS)? as u32)
+    }
+
+    fn update(conn: &Connection, query: Query) -> Result<u32, TableError> {
+        let query_string = &query.to_sql();
+
+        // let mut set_string = params
+        //     .iter()
+        //     .enumerate()
+        //     .map(|(i, (name, _))| format!("{} = ?{}", name, i + 1))
+        //     .collect::<Vec<String>>()
+        //     .concat();
+        // // Remove trailing comma
+        // set_string.truncate(set_string.len() - 1);
+        //
+        // let stmt_params = params.iter().map(|(_, val)| val).collect::<Vec<&String>>();
+        // let stmt_params: &[&String] = stmt_params.as_slice();
+        //
+        // let final_query = &(String::from("UPDATE notes\nSET ") + &set_string + query_string);
+        Ok(conn.execute(query_string, NO_PARAMS)? as u32)
+    }
+
+    fn get(conn: &Connection, query: Query) -> Result<Vec<Note>, TableError> {
+        let query_string = &query.to_sql();
+        Ok(conn
+            .prepare(query_string)?
+            .query_map(NO_PARAMS, |row| {
+                Ok(Note {
+                    title: row.get(1)?,
+                    created: row.get(2)?,
+                    text: row.get(3)?,
+                })
+            })?
+            .filter_map(Result::ok)
+            .collect())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn make_mock_db(conn: &Connection) {
+        conn.execute(
+            "CREATE TABLE IF NOT EXISTS notes (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                title TEXT NOT NULL UNIQUE,
+                created TEXT NOT NULL,
+                text TEXT NOT NULL
+                )",
+            NO_PARAMS,
+        )
+        .unwrap();
+
+        conn.execute(
+            "INSERT INTO notes (title, created, text) VALUES (?1, ?2, ?3)",
+            params![
+                String::from("Day 12"),
+                Local::now(),
+                String::from("Today's diary...")
+            ],
+        )
+        .unwrap();
+    }
+
+    #[test]
+    fn get_all() {
+        let mut conn = Connection::open("test.db").unwrap();
+        let tx = conn.transaction().unwrap();
+
+        make_mock_db(&tx);
+        let db_notes = Note::get_all(&tx).unwrap();
+
+        tx.rollback();
+
+        let note = Note::new("Day 12", "Today's diary...");
+        let db_note = db_notes.get(0).unwrap();
+
+        // Time will be different
+        assert_eq!(note.title, db_note.title);
+        assert_eq!(note.text, db_note.text);
+    }
+
+    #[test]
+    fn insert() {
+        let mut conn = Connection::open("test.db").unwrap();
+        let tx = conn.transaction().unwrap();
+
+        make_mock_db(&tx);
+        Note::insert(&tx, Note::new("Day 13", "A new diary entry...")).unwrap();
+        let db_notes = Note::get_all(&tx).unwrap();
+
+        tx.rollback();
+
+        let db_note = db_notes.get(1).unwrap();
+
+        assert_eq!(2, db_notes.len());
+        assert_eq!("Day 13", db_note.title);
+    }
+
+    #[test]
+    fn delete() {
+        let mut conn = Connection::open("test.db").unwrap();
+        let tx = conn.transaction().unwrap();
+
+        make_mock_db(&tx);
+        Note::insert(&tx, Note::new("Day 13", "A new diary entry...")).unwrap();
+        let note_to_delete = Note::get_all(&tx).unwrap().get(0).unwrap().clone();
+        Note::delete(&tx, note_to_delete).unwrap();
+        let db_notes = Note::get_all(&tx).unwrap();
+
+        tx.rollback();
+
+        let db_note = db_notes.get(0).unwrap();
+
+        assert_eq!(1, db_notes.len());
+        assert_eq!("Day 13", db_note.title);
+    }
+
+    // #[test]
+    // fn query() {
+    //     let mut conn = Connection::open("test.db").unwrap();
+    //     let tx = conn.transaction().unwrap();
+    //
+    //     make_mock_db(&tx);
+    //     Note::insert(&tx, Note::new("Day 13", "A new diary entry...")).unwrap();
+    //     Note::insert(&tx, Note::new("Day 14", "A new diary entry...")).unwrap();
+    //     Note::insert(&tx, Note::new("Day 15", "A new diary entry...")).unwrap();
+    //     let query = Query::new().add_where(Where::Equal("title", Field::Str("Day 14")));
+    //     let db_notes = Note::query(&tx, query).unwrap();
+    //
+    //     tx.rollback();
+    //
+    //     let db_note = db_notes.get(0).unwrap();
+    //
+    //     assert_eq!("Day 14", db_note.title);
+    // }
+}
