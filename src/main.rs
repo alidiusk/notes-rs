e84d475bb4f8d29fff7b9606b533baade60a897a
Liam Woodward <liamowoodward@gmail.com>
2020-05-15 23:57:16 -0400

    Finished initial transition to using async-std, sqlx, anyhow crates; trimmed code down a lot. However it is still very messy and needs a lot of work.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-// Get rid of dead code warnings for now
 #![allow(dead_code)]
 
-use crate::db::{DbContext, Field, Query, Table, Where};
-use crate::models::{Note, NoteTable};
+mod models;
+mod queries;
+
+use crate::models::Note;
+use crate::queries::*;
 
 use chrono::{DateTime, Local};
 use directories::ProjectDirs;
-use exitfailure::ExitFailure;
-use failure::ResultExt;
 use structopt::StructOpt;
 
 use std::path::{Path, PathBuf};
@@ -21,8 +21,13 @@ notes new "Day 12" "Today in my diary..."
 */
 
 #[derive(StructOpt, Debug)]
-#[structopt(name = "notes")]
-enum Opt {
+struct Args {
+    #[structopt(subcommand)]
+    cmd: Option<Command>,
+}
+
+#[derive(StructOpt, Debug)]
+enum Command {
     /// Create a new note.
     #[structopt(name = "new")]
     New {
@@ -60,63 +65,22 @@ enum Opt {
     },
     /// Delete an existing note.
     Delete {
-        /// Deletes all notes
-        #[structopt(short, long)]
-        all: bool,
         /// Only deletes notes that exactly match the designated title
         #[structopt(short, long, requires = "title")]
         exact: bool,
         /// Deletes first note that matches the title, or all notes that match the title if `all` is specified.
-        #[structopt(short, long, required_unless = "all")]
-        title: Option<String>,
+        #[structopt(short, long)]
+        title: String,
     },
 }
 
-fn get_notes_with_title(
-    db_context: &DbContext,
-    title: &str,
-    exact: bool,
-) -> Result<Vec<Note>, failure::Error> {
-    if exact {
-        let field = title.to_string();
-        let query = Query::new_get(&db_context.table, vec![title.to_owned()])
-            .add_where(Where::Equal("title".to_string(), Field::Str(field)));
-        NoteTable::get(&db_context, query)
-    } else {
-        let field = title.to_string();
-        let query = Query::new_get(&db_context.table, vec![title.to_owned()])
-            .add_where(Where::Like("title".to_string(), Field::Str(field)));
-        NoteTable::get(&db_context, query)
-    }
-}
-
-fn delete_notes_with_title(
-    db_context: &DbContext,
-    title: &str,
-    exact: bool,
-) -> Result<u32, failure::Error> {
-    if exact {
-        let field = title.to_string();
-        let query = Query::new_delete(&db_context.table)
-            .add_where(Where::Equal("title".to_string(), Field::Str(field)));
-        NoteTable::delete(&db_context, query)
-    } else {
-        let field = title.to_string();
-        let query = Query::new_delete(&db_context.table)
-            .add_where(Where::Like("title".to_string(), Field::Str(field)));
-        NoteTable::delete(&db_context, query)
-    }
-}
-
-fn file_is_dir<P: AsRef<Path>>(path: P) -> Result<bool, ExitFailure> {
-    let metadata = fs::metadata(path)
-        .with_context(|e| format!("could not determine if file is directory: {}", e))?;
+fn file_is_dir<P: AsRef<Path>>(path: P) -> anyhow::Result<bool> {
+    let metadata = fs::metadata(path)?;
     Ok(metadata.is_dir())
 }
 
-fn get_time_created<P: AsRef<Path>>(path: P) -> Result<DateTime<Local>, ExitFailure> {
-    let metadata =
-        fs::metadata(path).with_context(|e| format!("could not get file creation time: {}", e))?;
+fn get_time_created<P: AsRef<Path>>(path: P) -> anyhow::Result<DateTime<Local>> {
+    let metadata = fs::metadata(path)?;
     let time = metadata.created();
 
     match time {
@@ -125,229 +89,110 @@ fn get_time_created<P: AsRef<Path>>(path: P) -> Result<DateTime<Local>, ExitFail
     }
 }
 
-fn get_file_contents<P: AsRef<Path>>(path: P) -> Result<String, ExitFailure> {
-    let contents = fs::read_to_string(&path)
-        .with_context(|e| format!("could not get file contents: {}", e))?;
+fn get_file_contents<P: AsRef<Path>>(path: P) -> anyhow::Result<String> {
+    let contents = fs::read_to_string(&path)?;
     Ok(contents.trim().to_string())
 }
 
-fn main() -> Result<(), ExitFailure> {
-    let opt = Opt::from_args();
+#[async_std::main]
+async fn main() -> anyhow::Result<()> {
+    let args = Args::from_args();
 
-    let project_dir = match ProjectDirs::from("", "", "Notes") {
-        None => Err(failure::err_msg("Could not open local data directory."))
-            .context("Could not get access to database.")?,
-        Some(d) => d,
-    };
+    let project_dir = ProjectDirs::from("", "", "Notes").unwrap();
 
     let dir = project_dir.data_local_dir();
 
     if !dir.exists() {
-        fs::create_dir(dir).with_context(|_| {
-            format!(
-                "could not create local data directory: `{}`",
-                dir.to_str().unwrap()
-            )
-        })?;
+        fs::create_dir(dir)?;
     }
 
-    let path = dir.join("notes.db");
-    let db_context = DbContext::new(path, "notes".to_string())?;
+    let path = String::from("sqlite://") 
+        + &dir.join(dir).join("notes.db").to_str().unwrap().to_string();
+    // let path = std::env::var("DATABASE_URL")?;
 
-    NoteTable::init_db(&db_context)?;
+    let pool = sqlx::SqlitePool::new(&path).await?;
 
-    match opt {
-        Opt::New {
-            file: None,
-            editor: None,
-            title: Some(title),
-            text: Some(text),
-        } => {
-            // let tx = db_context.transaction()?;
-            NoteTable::insert(&db_context, Note::new(&title, &text))?;
-            // tx.commit()?;
-        }
-        Opt::New {
-            file: Some(file),
-            editor: None,
-            title: None,
-            text: None,
-        } => {
-            let path = env::current_dir()
-                .context("Could not find file")?
-                .join(file);
+    match args.cmd {
+        Some(Command::New { file, editor, title, text }) => {
+            let note = {
+                if let Some(path) = file {
+                    if file_is_dir(&path)? {
+                        panic!("File is a directory!");
+                    }
 
-            if file_is_dir(&path)? {
-                panic!("File is a directory!");
-            }
+                    let title = String::from(
+                        path.file_stem()
+                            .expect("Could not get file name")
+                            .to_str()
+                            .expect("Could not get file name")
+                    );
+                    let created = get_time_created(&path)?;
+                    let text = get_file_contents(&path)?;
 
-            // TODO: Remove expects later
-            let title = String::from(
-                path.file_stem()
-                    .expect("Could not get file name.")
-                    .to_str()
-                    .expect("Could not get file name."),
-            );
-            let created = get_time_created(&path)?;
-            let text = get_file_contents(&path)?;
+                    Note { title, created, text }
+                } else if let Some(path) = editor {
+                    let editor = env::var("EDITOR")?;
+                    let mut file_path = env::temp_dir();
+                    file_path.push("temp");
+                    fs::File::create(&file_path)?;
 
-            // let tx = db_context.transaction()?;
-            let note = Note {
-                title,
-                created,
-                text,
+                    process::Command::new(editor)
+                        .arg(&file_path)
+                        .status()?;
+
+                    let contents = fs::read_to_string(&file_path)?;
+
+                    // let tx = db_context.transaction()?;
+
+                    Note::new(path, contents)
+                } else {
+                    Note::new(title.unwrap(), text.unwrap())
+                }
             };
-            NoteTable::insert(&db_context, note)?;
-            // tx.commit()?;
-        }
-        Opt::New {
-            file: None,
-            editor: Some(title),
-            title: None,
-            text: None,
-        } => {
-            // https://stackoverflow.com/questions/56011927/how-do-i-use-rust-to-open-the-users-default-editor-and-get-the-edited-content
-            let editor = env::var("EDITOR")
-                .with_context(|_| "could not get default editor from environment")?;
-            let mut file_path = env::temp_dir();
-            file_path.push("temp");
-            fs::File::create(&file_path)
-                .with_context(|e| format!("could not create temporary file to edit: {}", e))?;
 
-            process::Command::new(editor)
-                .arg(&file_path)
-                .status()
-                .with_context(|e| format!("error editing file: {}", e))?;
+            insert_note(&pool, note).await?;
+        },
+        Some(Command::Get { all, exact, title }) => {
+            if all {
+                for note in get_all_notes(&pool).await? {
+                    println!("{}", note);
+                }
+            } else {
+                let notes = if exact {
+                    get_notes_exact(&pool, title.unwrap()).await?
+                } else {
+                    get_notes(&pool, title.unwrap()).await?
+                };
 
-            let contents = fs::read_to_string(&file_path)
-                .with_context(|e| format!("could not get file contents: {}", e))?;
-
-            // let tx = db_context.transaction()?;
-
-            let note = Note::new(&title, &contents);
-            NoteTable::insert(&db_context, note)?;
-
-            // tx.commit()?;
-        }
-        // Gets all notes with matching title
-        Opt::Get {
-            all: true,
-            exact,
-            title: Some(ref title),
-        } => {
-            let notes =
-                // not sure to keep with_context call heree
-                get_notes_with_title(&db_context, title, exact).with_context(|e| {
-                    format!("could not get notes matching title `{}`: {}", title, e)
-                })?;
-
-            if notes.is_empty() {
-                println!("No matches found.");
+                for note in notes {
+                    println!("{}", note);
+                }
             }
+        },
+        Some(Command::Edit { note_title, title, text }) => {
+            if let Some(original) = get_notes_exact(&pool, note_title.clone()).await?.get(0) {
+                let title = title.unwrap_or(original.title.clone());
+                let text = text.unwrap_or(original.text.clone());
+                let created = original.created;
+                let new_note = Note { title, created, text };
+
+                update_note(&pool, note_title, new_note).await?;
+            }
+        },
+        Some(Command::Delete { exact, title }) => {
+            if exact {
+                delete_note(&pool, title).await?;
+            } else {
+                delete_note_like(&pool, title).await?;
+            }
+        },
+        None => {
+            let notes = get_all_notes(&pool).await?;
 
             for note in notes {
                 println!("{}", note);
             }
         }
-        // Gets all notes
-        Opt::Get {
-            all: true,
-            exact: false,
-            title: None,
-        } => {
-            for row in NoteTable::get_all(&db_context)? {
-                println!("{}", row);
-            }
-        }
-        // Gets first note with matching title
-        Opt::Get {
-            all: false,
-            exact,
-            title: Some(ref title),
-        } => {
-            let notes =
-                // not sure to keep with_context call here
-                get_notes_with_title(&db_context, title, exact).with_context(|e| {
-                    format!("could not get first note matching title `{}`: {}", title, e)
-                })?;
-            if let Some(note) = notes.get(0) {
-                println!("{}", note);
-            } else {
-                return Err(failure::err_msg(format!(
-                    "No notes matching title `{}` found.",
-                    title
-                )))
-                .context("Could not get note.")?;
-            }
-        }
-        Opt::Edit {
-            note_title,
-            title,
-            text,
-        } => {
-            let params = match (title, text) {
-                (Some(tit), Some(tex)) => {
-                    vec![("title".to_string(), tit), ("text".to_string(), tex)]
-                }
-                (Some(tit), None) => vec![("title".to_string(), tit)],
-                (None, Some(tex)) => vec![("text".to_string(), tex)],
-                (None, None) => {
-                    return Err(failure::err_msg("No title or text to change given"))
-                        .context("Could not change text.")?;
-                }
-            };
-
-            let query = Query::new_update(&db_context.table, params)
-                .add_where(Where::Equal("title".to_string(), Field::Str(note_title)));
-
-            // let tx = db_context.transaction()?;
-
-            NoteTable::update(&db_context, query)?;
-
-            // tx.commit()?;
-        }
-        Opt::Delete {
-            all: true,
-            exact,
-            title: Some(title),
-        } => {
-            let num_deleted =
-                // not sure to keep with_context call here
-                delete_notes_with_title(&db_context, &title, exact).with_context(|e| {
-                    format!("could not delete notes matching title `{}`: {}", title, e)
-                })?;
-
-            println!("Deleted {} notes", num_deleted);
-        }
-        // Gets all notes
-        Opt::Delete {
-            all: true,
-            exact: false,
-            title: None,
-        } => {
-            for row in NoteTable::get_all(&db_context)? {
-                println!("{}", row);
-            }
-        }
-        // Gets first note with matching title
-        Opt::Delete {
-            all: false,
-            exact,
-            title: Some(title),
-        } => {
-            // not sure to keep with_context call here
-            let num_deleted =
-                delete_notes_with_title(&db_context, &title, exact).with_context(|e| {
-                    format!(
-                        "could not delete first note matching title `{}`: {}",
-                        title, e
-                    )
-                })?;
-
-            println!("Deleted {} notes.", num_deleted);
-        }
-        _ => Err(failure::err_msg("Could not execute command"))
-            .context("Invalid command entered.")?,
     }
 
     Ok(())
---
+++ src/queries.rs
@@ -0,0 +1,138 @@
+use chrono::{Local, DateTime};
+use sqlx::sqlite::SqlitePool;
+
+use crate::models::Note;
+
+pub async fn get_notes(pool: &SqlitePool, title: String) -> anyhow::Result<Vec<Note>> {
+    let notes = sqlx::query!(
+        r#"
+SELECT title, created, text
+FROM notes
+WHERE title LIKE $1
+        "#,
+        title
+    )
+    .fetch_all(pool)
+    .await?;
+
+    let notes: Vec<Note> = notes.into_iter()
+        .map(|rec| {
+            Note { 
+                title: rec.title,
+                created: rec.created.parse::<DateTime<Local>>().unwrap(),
+                text: rec.text,
+            }
+        }).collect();
+
+    Ok(notes)
+}
+
+pub async fn get_notes_exact(pool: &SqlitePool, title: String) -> anyhow::Result<Vec<Note>> {
+    let notes = sqlx::query!(
+        r#"
+SELECT title, created, text
+FROM notes
+WHERE title = $1
+        "#,
+        title
+    )
+    .fetch_all(pool)
+    .await?;
+
+    let notes: Vec<Note> = notes.into_iter()
+        .map(|rec| {
+            Note { 
+                title: rec.title,
+                created: rec.created.parse::<DateTime<Local>>().unwrap(),
+                text: rec.text,
+            }
+        }).collect();
+
+    Ok(notes)
+}
+
+pub async fn get_all_notes(pool: &SqlitePool) -> anyhow::Result<Vec<Note>> {
+    let notes = sqlx::query!(
+        r#"
+SELECT title, created, text
+FROM notes
+        "#
+    )
+    .fetch_all(pool)
+    .await?;
+
+    let notes: Vec<Note> = notes.into_iter()
+        .map(|rec| {
+            Note { 
+                title: rec.title,
+                created: rec.created.parse::<DateTime<Local>>().unwrap(),
+                text: rec.text,
+            }
+        }).collect();
+
+    Ok(notes)
+}
+
+pub async fn insert_note(pool: &SqlitePool, note: Note) -> anyhow::Result<()> {
+    sqlx::query!(
+        r#"
+INSERT INTO notes (title, created, text)
+VALUES ( $1, $2, $3 )
+        "#,
+        note.title,
+        note.created.to_string(),
+        note.text,
+    )
+    .execute(pool)
+    .await?;
+
+    Ok(())
+}
+
+pub async fn delete_note(pool: &SqlitePool, title: String) -> anyhow::Result<()> {
+    sqlx::query!(
+        r#"
+DELETE FROM notes
+WHERE title = $1
+        "#,
+        title
+    )
+    .execute(pool)
+    .await?;
+
+    Ok(())
+}
+
+pub async fn delete_note_like(pool: &SqlitePool, title: String) -> anyhow::Result<()> {
+    sqlx::query!(
+        r#"
+DELETE FROM notes
+WHERE title LIKE $1
+        "#,
+        title
+    )
+    .execute(pool)
+    .await?;
+
+    Ok(())
+}
+
+pub async fn update_note(pool: &SqlitePool, title: String, note: Note) -> anyhow::Result<()> {
+    sqlx::query!(
+        r#"
+UPDATE notes
+SET title = $1,
+    created = $2,
+    text = $3
+WHERE title = $4
+        "#,
+        note.title,
+        note.created.to_string(),
+        note.text,
+        title,
+    )
+    .execute(pool)
+    .await?;
+
+    Ok(())
+}
