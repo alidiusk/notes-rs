b252e2a14189479ade59b4a80ca3432cd0e8c9bf
Liam Woodward <liamowoodward@gmail.com>
2020-05-27 22:33:02 -0400

    Added more tests and test macros to make testing much easier.

--- tests/app.rs
+++ tests/app.rs
@@ -6,17 +6,21 @@ use assert_cmd::prelude::*;
 use predicates::prelude::*;
 use tempfile::{tempdir, TempDir};
 
-struct TestNotesFile {
-    path: PathBuf,
+struct TestCommand {
+    pub cmd: Command,
+    pub path: PathBuf,
     _tempdir: TempDir,
 }
 
-impl TestNotesFile {
+impl TestCommand {
     pub fn new() -> anyhow::Result<Self> {
         let tempdir = tempdir()?;
         let path = tempdir.path().to_owned().join("test");
+        let mut cmd = Command::cargo_bin("notes")?;
+        cmd.arg("--path").arg(path.as_os_str());
 
-        Ok(TestNotesFile {
+        Ok(TestCommand {
+            cmd,
             path,
             _tempdir: tempdir,
         })
@@ -31,15 +35,85 @@ impl TestNotesFile {
     }
 }
 
+macro_rules! cmd_with_args {
+    ($cmd:ident, [$($arg:expr),*]) => {{
+        let mut new_cmd = Command::cargo_bin("notes")?;
+        new_cmd.arg("--path").arg($cmd.path_as_os_str());
+
+        $(
+            new_cmd.arg($arg);
+        )*
+
+        $cmd.cmd = new_cmd;
+
+        $cmd
+    }};
+    ($($arg:expr),*) => {{
+        let mut cmd = TestCommand::new()?;
+
+        $(
+            cmd.cmd.arg($arg);
+        )*
+
+        cmd
+    }};
+}
+
+// Test macro that takes cmd inputs and the resulting predicates,
+// then expands to full test code that tests each input against its
+// predicate
+
+macro_rules! assert_success {
+    ($cmd:ident, $pred:expr) => {
+        $cmd.cmd.assert().success().stdout($pred)
+    };
+}
+
 #[test]
 fn init_notes_file() -> anyhow::Result<()> {
-    let file = TestNotesFile::new()?;
+    let mut cmd = cmd_with_args!();
+    assert_success!(cmd, predicate::str::contains("There are no notes."));
 
-    let mut cmd = Command::cargo_bin("notes")?;
-    cmd.arg("--path").arg(file.path_as_os_str());
-    cmd.assert()
-        .success()
-        .stdout(predicate::str::contains("There are no notes."));
+    Ok(())
+}
+
+#[test]
+fn get_all_notes_empty() -> anyhow::Result<()> {
+    let mut cmd = cmd_with_args!("get", "--all");
+    assert_success!(cmd, predicate::str::contains("There are no notes."));
+
+    Ok(())
+}
+
+#[test]
+fn one_new_note() -> anyhow::Result<()> {
+    let mut cmd = cmd_with_args!("new", "test");
+
+    assert_success!(cmd, predicate::str::contains("Note with ID 0 created."));
+
+    cmd = cmd_with_args!(cmd, ["get", "--all"]);
+
+    assert_success!(cmd, predicate::str::contains("test"));
+
+    Ok(())
+}
+
+#[test]
+fn two_new_notes() -> anyhow::Result<()> {
+    let mut cmd = cmd_with_args!("new", "test");
+
+    assert_success!(cmd, predicate::str::contains("Note with ID 0 created."));
+
+    cmd = cmd_with_args!(cmd, ["new", "second test"]);
+
+    assert_success!(cmd, predicate::str::contains("Note with ID 1 created."));
+
+    cmd = cmd_with_args!(cmd, ["get", "--all"]);
+
+    assert_success!(
+        cmd,
+        predicate::str::contains("test").and(predicate::str::contains("second test"))
+    );
 
     Ok(())
 }
